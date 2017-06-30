4d5594eca34769407161f4b3dceeb7e27acde91c
Jochum Börger <jborger@xebia.com>
2017-07-01 00:04:56 +0200

    Solution day 6, part 2

--- 6/solution.exs
+++ 6/solution.exs
@@ -26,6 +26,16 @@ defmodule LightGrid do
   defp switch(:on, coord, map), do: Map.put(map, coord, true)
   defp switch(:off, coord, map), do: Map.put(map, coord, false)
   defp switch(:toggle, coord, map), do: Map.update(map, coord, true, &(!&1))
+
+  def execute_v2({cmd, fx, fy, tx, ty}, map) do
+    coords(fx, fy, tx, ty)
+      |> Enum.reduce(map, fn (x, acc) -> switch_v2(cmd, x, acc) end )
+  end
+
+  defp switch_v2(:on, coord, map), do: Map.update(map, coord, 1, &(&1+1))
+  defp switch_v2(:off, coord, map), do: Map.update(map, coord, 0, 
+      fn (x) -> if x > 0, do: x-1, else: 0 end )
+  defp switch_v2(:toggle, coord, map), do: Map.update(map, coord, 2, &(&1+2))
 end
 
 {:ok, file} = File.open("input", [:read])
@@ -37,3 +47,13 @@ final = IO.stream(file, :line)
 File.close(file)
 
 IO.puts "Number of lights on: #{Enum.count(final)}"
+
+{:ok, file} = File.open("input", [:read])
+brightness = IO.stream(file, :line)
+    |> Stream.map(&String.rstrip/1)
+    |> Stream.map(&LightGrid.parse_command/1)
+    |> Enum.reduce(%{}, &LightGrid.execute_v2/2)
+    |> Enum.reduce(0, fn ({_, x}, acc) -> x + acc end)
+File.close(file)
+
+IO.puts "Brightness level is: #{brightness}"
